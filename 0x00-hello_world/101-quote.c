dfc6b10c57dd32cdce03feaf553294461b31d607
Samuelibudi <ibudisamuel@gmail.com>
2022-09-08 09:53:44 -0700

    Fixing file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char strpt[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+	char strpt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, strpt, 59);
 	return(1);
