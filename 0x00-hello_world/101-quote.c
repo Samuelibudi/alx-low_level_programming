fe0164a1ce0861234c264ba2922d5497d1104244
Samuelibudi <ibudisamuel@gmail.com>
2022-09-08 09:46:07 -0700

    Adding file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	char strpt[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+
+	write(2, strpt, 59);
+	return(1);
+}
