f481e40af169513d7d945c52d71a4b25c116c323
Samuelibudi <ibudisamuel@gmail.com>
2022-09-09 06:15:11 -0700

    Adding file 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Program entry point
+ *
+ * Program prints alphabet in lower case and reverse.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
