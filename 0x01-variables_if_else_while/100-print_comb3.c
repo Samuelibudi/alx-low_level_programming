53c2861c51df62b8f0dad070afa326213427a20d
Samuelibudi <ibudisamuel@gmail.com>
2022-09-09 06:49:56 -0700

    Adding file 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Program prints between 00 to 99.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m, n;
+
+	m = 48;
+	n = 48;
+
+	while (n < 48)
+	{
+		m = 48;
+		while (m < 58)
+		{
+			putchar(n);
+			putchar(m);
+			if (m == 57 && n ==57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			m++;
+		}
+		n++;
+	}
+
+	putchar ('\n');
+	return (0);
+}
