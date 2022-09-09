9acb563c7e1aed9f0d75efef85721e3e67d3e5ca
Samuelibudi <ibudisamuel@gmail.com>
2022-09-09 06:02:25 -0700

    Adding file 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Program entry point
+ *
+ * Program to print numbers from 0 to 9.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+
+	putchar('\n')
+
+	return (0);
+}
