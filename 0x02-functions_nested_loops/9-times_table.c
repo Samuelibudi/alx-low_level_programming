6a1224d555e4b7612e41c1afb7091ab7cbb3966a
Samuelibudi <ibudisamuel@gmail.com>
2022-09-13 08:20:50 -0700

    Fixing file 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,18 +7,26 @@
  */
 void times_table(void)
 {
-	int m, n, pdt;
+	int m, n, pdt, op;
 
 	for (m = 0; m < 10; m++)
 	{
 		for (n = 0; n < 10; n++)
 		{
 			pdt = n * m;
-			pdt = pdt + 48;
+			if (pdt < 10)
+			{
+				pdt = pdt + 48;
 
-			_putchar(pdt);
-			_putchar(',');
-			_putchar(' ');
+				_putchar(pdt);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((pdt / 10) + 48);
+				_putchar((pdt % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}
