cc7a512420f2fd9c7b6fe9ebb6b27d28f6e067df
Samuelibudi <ibudisamuel@gmail.com>
2022-09-13 06:37:51 -0700

    Adding file 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is alphabet character.
+ *
+ * @c: parameter to be checked.
+ *
+ * Return: 1 if c is letter and 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 123) || (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
