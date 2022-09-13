22991e2a2d2a3205428869c31157a0e967ef824f
Samuelibudi <ibudisamuel@gmail.com>
2022-09-13 06:24:23 -0700

    Fixing file to betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lower case.
- * 
+ * print_alphabet_x10 - Prints the alphabet in lower case.
+ *
  * Displays it 10 times.
  *
  * Return:Always 0
@@ -10,13 +10,13 @@
 
 void print_alphabet_x10(void)
 {
-	int i, count;
+int i, count;
 
-	for (count = 0; count < 10; count++)
-	{
-		for (i = 97; i <= 122; i++)
-			_putchar(i);
+for (count = 0; count < 10; count++)
+{
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
 
-	_putchar('\n');
-	}
+_putchar('\n');
+}
 }
