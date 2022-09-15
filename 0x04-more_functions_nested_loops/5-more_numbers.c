8140d155c0c79e79eef9a299e614219b40bcb46f
Samuelibudi <ibudisamuel@gmail.com>
2022-09-15 05:23:36 -0700

    Adding file 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, count;
+for (count = 0; count < 10; count++)
+{
+	for (i = 0; i < 15; i++)
+	{
+		if (i < 10)
+			_putchar((i % 10) + 48);
+		_putchar((i / 10) + 48);
+	}
+	_putchar('\n');
+}
+}
