752130c5c104588dca8e2e8336a5e3531cb6f71c
Samuelibudi <ibudisamuel@gmail.com>
2022-09-15 07:10:26 -0700

    Fixing file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,12 +17,12 @@ int main(void)
 
 	for (i = 2; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) != 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
 			printf(" Fizz");
-		else if ((i % 3) != 0 && (i % 5) == 0)
+		else if (i % 5 == 0)
 			printf(" Buzz");
-		else if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz ");
 		else
 			printf(" %d", i);
 	}
