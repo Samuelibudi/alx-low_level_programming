908b10d9b7929a8000064b046212b3410793fc3a
Samuelibudi <ibudisamuel@gmail.com>
2022-09-15 08:30:26 -0700

    Adding file 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print number - prints an integer.
+ * @n: Parameter to print.
+ * Return: nothing.
+ */
+void print_number(int n)
+{
+	unsigned int i, e, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	e = i;
+	count = 1;
+
+	while (e > 9)
+	{
+		e /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
+}
