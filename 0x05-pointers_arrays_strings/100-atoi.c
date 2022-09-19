2b96e1ad56d466d848a4644688ef707330f34eea
Samuelibudi <ibudisamuel@gmail.com>
2022-09-19 12:58:30 -0700

    Adding file 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+nclude "main.h"
+
+/**
+ * _atoi - function converts string to integer.
+ * @s: string to convert.
+ * Return: returns integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, p = 0, n = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			n *= -1;
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+	for (i = count - size; i < count; i++)
+	{
+		p = p + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (p * n);
+}
