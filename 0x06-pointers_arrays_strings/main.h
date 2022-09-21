0369f749c5fd3aa0e0ffa4712f3bcb7fce9d8271
Samuelibudi <ibudisamuel@gmail.com>
2022-09-21 11:34:21 -0700

    Fixing file main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef _main_h_
 #define _main_h_
 
-int _putchar(char);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
