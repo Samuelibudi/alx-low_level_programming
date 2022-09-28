141f7790118256a3ac2c1e2a9753f003a753d28a
Samuelibudi <ibudisamuel@gmail.com>
2022-09-28 03:28:59 -0700

    Fixing file 2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s != '\0')
-	{
-		i++;
-		_strlen_recursion(s + 1);
-	}
-	return (i);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
