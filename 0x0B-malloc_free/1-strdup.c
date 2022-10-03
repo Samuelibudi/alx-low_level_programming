b018ed2230360385dabb5368c0757fcb2f5f7291
Samuelibudi <ibudisamuel@gmail.com>
2022-10-03 08:58:24 -0700

    Adding file 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strlen - function returns length of string.
+ * @s: pointer to string
+ * Return: string length.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strdup - Copies a string to allocated memory.
+ * @str: Pointer to string to copy.
+ * Return: A pointer to copy of string.
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = _strlen(str);
+	int count = 0;
+	char *c;
+
+	c = malloc(sizeof(char) * i);
+
+	while (count >= 0)
+	{
+		*(c + count) = *(str + i);
+		if (*(str + count) == '\0')
+			break;
+		count++;
+	}
+	return (c);
+}
+
