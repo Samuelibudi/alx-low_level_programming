4e352e3384cde7b4af259feb715f5fb787e86e1a
Samuelibudi <ibudisamuel@gmail.com>
2022-10-06 04:21:50 -0700

    Fixing errors in 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, count;
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = "";
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		;
