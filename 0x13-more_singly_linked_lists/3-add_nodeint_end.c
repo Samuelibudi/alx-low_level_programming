5ce4f35cb5e50c550a51c6f662d6036e25deffa7
Samuelibudi <ibudisamuel@gmail.com>
2022-10-24 10:46:57 -0700

    Adding file 3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - function adds a node at the end.
+ * @head: head of list.
+ * @n: number of elements.
+ * Return: Addr of new element. Null if failed.
+ */
+lsitint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *tp;
+
+	(void)tp;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	tp = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (tp->next != NULL)
+		{
+			tp = tp->next;
+		}
+		tp->next = new;
+	}
+	return (*head);
+}
