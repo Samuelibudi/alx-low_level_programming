b535a6856b31e88a2504ad8fe519330ee051783e
Samuelibudi <ibudisamuel@gmail.com>
2022-10-27 04:23:40 -0700

    Adding file 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - return the number of bits ypu would need to flip to
+ * get form one number t,o another.
+ * @n: number one
+ * @m: number two
+ * Return: number of bits.
+ */
+unsigned int flip_bits(unsigned long in n, unsigned long int m)
+{
+	unsigned int i;
+
+	for (i = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			i++;
+	}
+
+	return (i);
+}
