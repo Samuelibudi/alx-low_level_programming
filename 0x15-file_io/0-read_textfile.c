054e2b08813d92d2aef5f6f66cb29adf9f8a5aba
Samuelibudi <ibudisamuel@gmail.com>
2022-10-31 02:05:00 -0700

    Adding files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * read_textfile - Function reads a text file and prints to POSIX.
+ * @filename: Pointer to file to print.
+ * @letters: Number of letters to print.
+ * Return: Actual number of letters it could print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	char *buf;
+	ssize_t nr, nw;
+
+	if (!filename)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nr = read(f, buf, letters);
+	nw = write(STDOUT_FILENO, buf, nr);
+
+	close(f);
+
+	free(buf);
+
+	return (nw);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+The following tasks were undertaken as part of the alx software engineering program to acquit learners with aspects of file inputs and ouputs in c language.
+
+Task 0 - A function that reads a text file and prints it to the POSIX standard output.
+Task 1 - A function that creates a file.
+Task 2 - A function that appends text at the end of a file.
+Task 3 - A program that copies the content of a file to another file.
+Task 4 - A program that displays the information contained in the ELF header at the start of an ELF file.
